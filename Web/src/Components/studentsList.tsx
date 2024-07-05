a2c67408fea30b7f81a7c99d85dea7215fbf51d6
debraj-06 <rajdeb2522a@gmail.com>
2024-07-05 21:19:54 +0530

    added serach bar at student list just UI not in backend

--- Web/src/Components/studentsList.tsx
+++ Web/src/Components/studentsList.tsx
@@ -11,7 +11,8 @@ import studentList1 from '../Asets/contsant';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { useState } from 'react';
-import { Box } from '@mui/material';
+import { alpha, AppBar, Box, IconButton, InputBase, styled, Toolbar, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface Column {
     id: 'name' | '_id' | 'phone' | 'email'|'dob';
@@ -84,12 +85,87 @@ export default function StudentsList() {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
-
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+      
+      const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        width: '100%',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          transition: theme.transitions.create('width'),
+          [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+              width: '20ch',
+            },
+          },
+        },
+      }));
     return (
         <Box className="py-5 bg-cyan-500 hover:bg-red-600">
+            <Box  sx={{ flexGrow: 1 }} >
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon/> */}
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Here To Search....
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+   
         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
             <TableContainer sx={{ maxHeight: 440 }}>
+
                 <Table stickyHeader aria-label="sticky table">
+                
                     <TableHead>
                         <TableRow>
                             {columns.map((column) => (
