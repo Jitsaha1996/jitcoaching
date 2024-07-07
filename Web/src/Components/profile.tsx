9b2fd2445163341da69362fc8633f3498a14b4e1
suparnadey-5 <miss.s.dey05@gmail.com>
2024-07-07 17:41:42 +0530

    achievement

--- Web/src/Components/profile.tsx
+++ Web/src/Components/profile.tsx
@@ -8,39 +8,49 @@ export const MyProfile = () => {
     console.log(state);
   return (
     <Box className="flex flex-col  w-full max-w-25 justify-center items-center  bg-transparent">
-        <Box className=' flex justify-center items-center pt-2' >
-        <Typography className="flex text-4xl font-extrabold  ">
-          Hi   {myProfile.name},Welcome to Jit Coachine Center 
-        </Typography>
-        </Box>
+        
         <Box className='flex justify-start items-start pt-2 w-full'>
           <Box className='w-1/3'>
           <img src={myProfile?.pic} alt='test'/>
           </Box>
             <Box className='w-2/3 bg-slate-200 ml-5 pl-5'>
             <Typography className="text-4xl font-light  ">
-          Hi   {myProfile.name},Welcome to Jit Coachine Center 
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis repudiandae facere quibusdam nesciunt enim eaque, at officiis repellendus excepturi
-           tenetur nisi dignissimos similique perspiciatis repellat quos voluptatibus autem natus obcaecati.
-        </Typography>
+                 Hi   {myProfile.name},Welcome to Jit Coachine Center 
+         
+            </Typography>
             </Box>
-          
-
         </Box>
+
         <Box className= 'flex flex-col w-full '>
-          <Box className= 'flex justify-center items-center py-5'>
+          <Box className= 'flex justify-center items-center text-5xl py-0 bg-slate-500 '>
           <Typography >
             Achievements
           </Typography>
           </Box>
-          <Box className='flex py-5'>
-          <Typography >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Cumque est tempore saepe ducimus ea sint in? Reiciendis veniam ex doloremque, porro sed autem amet voluptatibus fugiat optio nesciunt! Non, accusantium.
-          </Typography>
-          </Box>
+          
+          <table className="flex flex-col justify-center place-content-center py-3 border border-separate  border-slate-600 border-spacing-5 w-full">
+            <thead>
+              <tr className='flex justify-center  ' > 
+                <th className= "border  border-slate-600 ...">Standard </th>
+                <th className= "border  border-slate-600 ...">Marks</th>
+              </tr>
+            </thead>
 
-</Box>
+            <tbody>
+               <tr className='flex justify-center '>
+                <td className= "border  border-slate-600 ...">10th=</td>
+                <td className= "border  border-slate-600 ...">87.5%</td>
+              </tr>
+              <tr className='flex justify-center '>
+                <td className= "border  border-slate-600 ...">12th=</td>
+                <td className= "border  border-slate-600 ...">78.5%</td>
+              </tr>
+              
+            </tbody>
+          </table>
+          
+
+      </Box>
     </Box>
   )
 }
