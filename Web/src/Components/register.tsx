b5f39dc70325d510661755cf9e542c56aafa1082
Jitsaha1996 <sahajit1996@gmail.com>
2024-07-08 22:29:23 +0530

    update

--- Web/src/Components/register.tsx
+++ Web/src/Components/register.tsx
@@ -1,19 +1,24 @@
 import { Alert, Box, Button, TextField, Typography } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import CheckIcon from '@mui/icons-material/Check';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import UserContext from '../Auth/userContext';
 
 export const Register = () => {
+  const { login } = useContext<any>(UserContext);
+  const navigate = useNavigate();
+
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
     phone: "",
     dob: "",
-    pic: ""
+    pic: "",
+    status: ""
   });
 
   const [isError, setIsError] = useState(false);
@@ -26,10 +31,14 @@ export const Register = () => {
       )
         .then(function (response: any) {
           console.log("response", response);
+          setData({ ...data, status: "Sucess" });
+          login(response.data);
+          navigate('/profile')
 
         })
         .catch(function (error) {
           console.log(error);
+          setData({ ...data, status: "Error" });
           setIsError(true);
           setError(error?.message);
 
@@ -43,33 +52,47 @@ export const Register = () => {
   };
 
   const setFileBase64 = (files: any) => {
+    console.log("Deatils",files);
     let idCardBase64 = '';
-    getBase64(files, (result:any) => {
+    getBase64(files, (result: any) => {
       idCardBase64 = result;
       console.log("Files", idCardBase64);
-      setData({...data,pic:idCardBase64});
+      setData({ ...data, pic: idCardBase64 });
 
     });
   }
-  const getBase64=(file:any, cb:any)=> {
+  const getBase64 = (file: any, cb: any) => {
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
-        cb(reader.result)
+      cb(reader.result)
     };
     reader.onerror = function (error) {
-        console.log('Error: ', error);
+      console.log('Error: ', error);
     };
-}
+  }
   return (
     <>
       <CssBaseline />
       <Container maxWidth="sm">
 
         <Box className="flex flex-col pt-3" sx={{ bgcolor: '#cfe8fc', height: '100vh' }} component="form" onSubmit={handleSubmit} noValidate>
-          <Box>
+          {/* <Box>
             {isError ? <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">
               {error}
+            </Alert> : null}
+
+
+            <Typography className='flex text-4xl font-extrabold dark:text-white justify-center items-center pt-2 '>
+              Register
+            </Typography>
+          </Box> */}
+          <Box>
+            {data?.status === "Error" ? <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">
+              {error}
+            </Alert> : null}
+            {data?.status === "Sucess" ? <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+              Register Sucess!!
             </Alert> : null}
 
 
